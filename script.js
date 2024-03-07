b4db217aa0f04541559d657e4734726d47de12d1
sherzodboy <kholmamatovsh09@gmail.com>
2024-03-07 08:44:52 +0500

    Resetting game update

--- script.js
+++ script.js
@@ -13,14 +13,30 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // initial conditons
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = () => {
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+
+  diceEl.classList.add('hidden');
+};
+init();
 
 const switchPlayer = () => {
   currentScore = 0;
@@ -61,7 +77,8 @@ btnHold.addEventListener('click', function () {
     document.querySelector(`#score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 20) {
+    // if score higher 100 finish game
+    if (scores[activePlayer] >= 100) {
       // finish game
       playing = false;
       document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -78,3 +95,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
